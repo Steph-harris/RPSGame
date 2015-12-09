24a74cee56b0efab9141e10732f80528544283ae
steph-harris <steph.harris21@gmail.com>
2015-12-09 01:13:03 -0500

    added base js with alerts on click

--- scripts/RPSCounters.js
+++ scripts/RPSCounters.js
@@ -1,7 +1,28 @@
+window.rps = {
+  gameState: {
+    usrScr : 0,
+    compScr : 0,
+    rndCnt : 1
+  }
+}
+
 $(document).ready(function(){
   var rps = ["rock", "paper", "scissors"];
-  var usrChoice = Math.floor(Math.random() * rps.length);
+  var usrChoice = $(".btn-success").text();
+  var compChoice = Math.floor(Math.random() * rps.length);
 
-  console.log(usrChoice)
-
+  $("#rock").on("click", function(e){
+    e.preventDefault()
+    alert("You picked rock and the comp chose: " +rps[compChoice]);
+  })
+  $("#paper").on("click", function(e){
+    e.preventDefault()
+    alert("You picked paper and the comp chose: " +rps[compChoice]);
+  })
+  $("#scissors").on("click", function(e){
+    e.preventDefault()
+    alert("You picked scissors and the comp chose: " +rps[compChoice]);
+  })
 });
+
+//user chooses r,p, or s, so make conditionals for each button?
