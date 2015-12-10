46577c88ceae7ae0b1da382b9f7bb7d3c5b33e99
steph-harris <steph.harris21@gmail.com>
2015-12-10 00:21:57 -0500

    added button and function for generating comp choice

--- scripts/RPSCounters.js
+++ scripts/RPSCounters.js
@@ -9,20 +9,31 @@ window.rps = {
 $(document).ready(function(){
   var rps = ["rock", "paper", "scissors"];
   var usrChoice = $(".btn-success").text();
-  var compChoice = Math.floor(Math.random() * rps.length);
+  //var compChoice = Math.floor(Math.random() * rps.length);
 
   $("#rock").on("click", function(e){
     e.preventDefault()
-    alert("You picked rock and the comp chose: " +rps[compChoice]);
+    alert("You picked rock and the comp chose: " + compRPS());
   })
   $("#paper").on("click", function(e){
     e.preventDefault()
-    alert("You picked paper and the comp chose: " +rps[compChoice]);
+    alert("You picked paper and the comp chose: " + compRPS());
   })
   $("#scissors").on("click", function(e){
     e.preventDefault()
-    alert("You picked scissors and the comp chose: " +rps[compChoice]);
+    alert("You picked scissors and the comp chose: " + compRPS());
   })
+
+  $("#rand").on("click",function(){
+    compRPS();
+  })
+
+  function compRPS(){
+    var compChoice = Math.floor(Math.random() * rps.length);
+    $("h2").text(rps[compChoice])
+
+    return (rps[compChoice])
+  }
 });
 
 //user chooses r,p, or s, so make conditionals for each button?
