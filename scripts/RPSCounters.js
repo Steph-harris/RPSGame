595708a802757ef97657e0fe4273a127dfe8c08c
steph-harris <steph.harris21@gmail.com>
2015-12-10 21:30:47 -0500

    limited game to 5 rounds

--- scripts/RPSCounters.js
+++ scripts/RPSCounters.js
@@ -9,34 +9,34 @@ window.roPaSc = {
 $(document).ready(function(){
   var rps = ["Rock", "Paper", "Scissors"];
   
-  
   $(".btn-success").on("click", function(e){
     e.preventDefault()
     var usrChoice = $(this).attr("id");      
     var computerChoice = compRPS();
 
-    console.log(usrChoice, computerChoice);
-    if(usrChoice===computerChoice){
-      $("#myModalLabel").text("Tie Game!")
-      $(".modal-body").text("You both picked: " + computerChoice + "|" + usrChoice + "; tie game means no points! Play Again!")
-      roPaSc.gameState.rndCnt--;
-    } else if(usrChoice==="Rock" && computerChoice==="Scissors" || usrChoice==="Paper" && computerChoice==="Rock" || usrChoice==="Scissors" && computerChoice==="Paper"){
-      $("#myModalLabel").text("You may have won this round...")
-      $(".modal-body").text("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for you!");
-      roPaSc.gameState.usrScr++;
-      $("#userScore").text("Your Score is: " + roPaSc.gameState.usrScr)
-    }else if(usrChoice==="Rock" && computerChoice==="Paper" || usrChoice==="Paper" && computerChoice==="Scissors" || usrChoice==="Scissors" && computerChoice==="Rock"){
-      $("#myModalLabel").text("Rise of the Machines...You Lose")
-      $(".modal-body").text("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for the Computer!");
-      roPaSc.gameState.compScr++;
-      $("#compScore").text("Computer Score is: " + roPaSc.gameState.compScr)
+    if(roPaSc.gameState.rndCnt<=5){
+      console.log(usrChoice, computerChoice);
+      if(usrChoice===computerChoice){
+        $("#myModalLabel").text("Tie Game!")
+        $(".modal-body").text("You both picked: " + computerChoice + "|" + usrChoice + "; tie game means no points! Play Again!")
+        roPaSc.gameState.rndCnt--;
+      } else if(usrChoice==="Rock" && computerChoice==="Scissors" || usrChoice==="Paper" && computerChoice==="Rock" || usrChoice==="Scissors" && computerChoice==="Paper"){
+        $("#myModalLabel").text("You may have won this round...")
+        $(".modal-body").text("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for you!");
+        roPaSc.gameState.usrScr++;
+        $("#userScore").text("Your Score is: " + roPaSc.gameState.usrScr)
+      }else if(usrChoice==="Rock" && computerChoice==="Paper" || usrChoice==="Paper" && computerChoice==="Scissors" || usrChoice==="Scissors" && computerChoice==="Rock"){
+        $("#myModalLabel").text("Rise of the Machines...You Lose")
+        $(".modal-body").text("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for the Computer!");
+        roPaSc.gameState.compScr++;
+        $("#compScore").text("Computer Score is: " + roPaSc.gameState.compScr)
+      }
+      $("#myModal").modal("show");
     }
-    $("#myModal").modal("show");
+    else{
+      alert("game over")
+    }
   })
-
-  $("#round").text("Round " + roPaSc.gameState.rndCnt)
-  $("#userScore").text("Your Score is " + roPaSc.gameState.usrScr)
-  $("#compScore").text("Computer Score is " + roPaSc.gameState.compScr)
 
   function compRPS(){
     var compChoice = Math.floor(Math.random() * rps.length);
--- scripts/RPSLogic.js
+++ scripts/RPSLogic.js
@@ -5,5 +5,8 @@ $(document).ready(function(){
     $("#round").text("Round " + roPaSc.gameState.rndCnt)
   });
 
-
+  $("#round").text("Round " + roPaSc.gameState.rndCnt)
+  $("#userScore").text("Your Score is " + roPaSc.gameState.usrScr)
+  $("#compScore").text("Computer Score is " + roPaSc.gameState.compScr)
 });
+
