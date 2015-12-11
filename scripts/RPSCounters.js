74347f152332c80467f2759263f4f15242f7876a
steph-harris <steph.harris21@gmail.com>
2015-12-10 20:53:44 -0500

    added all RPS possible outcomes

--- scripts/RPSCounters.js
+++ scripts/RPSCounters.js
@@ -17,17 +17,21 @@ $(document).ready(function(){
 
     console.log(usrChoice, computerChoice);
     if(usrChoice===computerChoice){
-      alert("You both picked: " + computerChoice + "|" + usrChoice + "; tie game means no points! Play Again!")
+      $("#myModalLabel").text("Tie Game!")
+      $(".modal-body").text("You both picked: " + computerChoice + "|" + usrChoice + "; tie game means no points! Play Again!")
       roPaSc.gameState.rndCnt--;
     } else if(usrChoice==="Rock" && computerChoice==="Scissors" || usrChoice==="Paper" && computerChoice==="Rock" || usrChoice==="Scissors" && computerChoice==="Paper"){
-      alert("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for you!");
+      $("#myModalLabel").text("You may have won this round...")
+      $(".modal-body").text("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for you!");
       roPaSc.gameState.usrScr++;
       $("#userScore").text("Your Score is: " + roPaSc.gameState.usrScr)
     }else if(usrChoice==="Rock" && computerChoice==="Paper" || usrChoice==="Paper" && computerChoice==="Scissors" || usrChoice==="Scissors" && computerChoice==="Rock"){
-      alert("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for the Comp!");
+      $("#myModalLabel").text("Rise of the Machines...You Lose")
+      $(".modal-body").text("You picked " + usrChoice + " and the comp chose " + computerChoice +". 1 point for the Computer!");
       roPaSc.gameState.compScr++;
       $("#compScore").text("Computer Score is: " + roPaSc.gameState.compScr)
     }
+    $("#myModal").modal("show");
   })
 
   $("#round").text("Round " + roPaSc.gameState.rndCnt)
