56b42c2b6c12dfe5051dbc6e15ad4636140e6e1e
steph-harris <steph.harris21@gmail.com>
2015-12-13 00:11:49 -0500

    created branch to experiment with firebase inputs

--- scripts/RPSCounters.js
+++ scripts/RPSCounters.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
   });
   $(".btn-success").on("click", function(e){
       e.preventDefault()
-      
+      debugger;
       var usrChoice = $(this).attr("id");      
       var computerChoice = compRPS();
 
